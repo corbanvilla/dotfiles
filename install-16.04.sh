880a85edae66dc48c2d9daa89ef61afeb007ac9f
Animcogn <corbanvilla@gmail.com>
2018-09-08 13:45:16 -0600

    added autoconf to install

--- install-16.04.sh
+++ install-16.04.sh
@@ -48,11 +48,11 @@ fi
 if [ "$DO_DEPENDENCY_INSTALLS" == "TRUE" ] ; then
 	echo "downloading i3-gaps dependencies..."
 	sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm-dev 
-  sudo apt install curl
+  	sudo apt install curl autoconf
   
-  sudo add-apt-repository ppa:aguignard/ppa
-  sudo apt-get update
-  sudo apt-get install libxcb-xrm-dev
+  	sudo add-apt-repository ppa:aguignard/ppa
+  	sudo apt-get update
+  	sudo apt-get install libxcb-xrm-dev
 fi
 
 #compile and download i3 all in one function! what a time to be alive!
